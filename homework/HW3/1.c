7591dfae438ae7d4a5cd90ea589bd43b43ec1b92
Idhibhat Pankam <marginpankam@gmail.com>
2025-01-21 15:24:59 +0700

    2

--- homework/HW3/1.c
+++ homework/HW3/1.c
@@ -2,15 +2,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 int main() {
+    int n = 3;
     int i;
-    int n;
     pid_t childpid;
-    n = 4;
     for (i = 0; i < n; ++i) {
-        childpid = fork() ;
+        childpid = fork();
         if (childpid > 0)
             break;
     }
+    if(i == 0)
+        printf("I am the parent process. My PID is %d\n", (long) getpid());
+    else if(i == 1)
+        printf("I am the child process. My PID is %d and my parent's PID is %d\n", (long) getpid(), (long) getppid());
+    else if(i == 2)
+        printf("I am the grandchild process. My PID is %d and my parent's PID is %d\n", (long) getpid(), (long) getppid());
     wait(0);
-    printf("This is process %ld with parent %ld\n",(long) getpid(), (long) getppid());
 }
